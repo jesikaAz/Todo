38036ddb99ec673db41276a48ac8d4d57122f16f
jesikaAz2017 <j.anschutz@laposte.net>
2018-06-11 21:10:52 +0200

    Add css
    Add todomvc exemple

---
+++ src/components/Todos.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="todoapp">
+        <header class="header">
+            <h1 class="Todos">Todos</h1>
+        </header>
+    </section>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style src="../assets/todos.css"></style>
+
+
